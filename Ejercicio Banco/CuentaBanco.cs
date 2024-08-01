b9d912e9ca9db368e9d02e74c3f4c252c36dbcea
Marcelo Torres <kevin.torres85@outlook.es>
2024-07-31 20:21:33 -0500

    ejercicio en clase

---
+++ Ejercicio Banco/CuentaBanco.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Banco
+{
+    internal class CuentaBanco
+    {
+        //atributos
+        public string nombre;
+        public string apellido;
+        public double saldo;
+
+        //constructor
+        public CuentaBanco (string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.saldo = 50.0;
+        }
+        //para ver información
+        public void Informacion()
+        {
+            Console.WriteLine($"Hola {nombre} {apellido}. ¡¡¡Bienvenido!!!");
+        }
+
+        //para depositar
+        public void Depositar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                saldo += cantidad;
+                Console.WriteLine($"\nSe ha depositado ${cantidad}.\nSaldo actual:${saldo}");
+            }
+            else
+            {
+                Console.WriteLine("La cantidad para depositar debe ser mayor a $0 (cero).");
+            }
+        }
+
+        //para retirar
+        public void Retirar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                if (cantidad <= saldo)
+                {
+                    saldo -= cantidad;
+                    Console.WriteLine($"\nSe ha retirado ${cantidad}.\nSaldo actual: ${saldo}");
+                }
+                else
+                {
+                    Console.WriteLine("\nFondos Insuficientes...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nLa cantidad para retirar debe ser mayor a $0 (cero).");
+            }
+        }
+        //para consultar saldo
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"\nSaldo final: ${saldo}");
+        }
+    }
+}
---
+++ Ejercicio Banco/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Banco
+{
+    internal class Program
+    {
+        private static string apellido;
+
+        /*Ejercicio simular una cuenta de Banco
+* 1. Crear la clase CuentaBnaco
+* 2. Declarar variables
+* nombre, apellido(atributos vacios), 50$ (definido)
+* 3. metodos
+* depositar, retirar, consultarSaldo*/
+        static void Main(string[] args)
+        {
+            //Se solicita información
+            Console.Write("Ingreses su nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su apellido: ");
+            string apellido = Console.ReadLine();
+
+            // Crear una cuenta bancaria
+
+            CuentaBanco cuenta = new CuentaBanco(nombre, apellido);
+
+            // Mostrar información del titular de la cuenta
+
+            cuenta.Informacion();
+
+            // Consultar saldo inicial
+            cuenta.ConsultarSaldo();
+
+            // Depositar dinero
+            Console.Write("\nIngrese la cantidad a depositar: $");
+            double deposito = double.Parse(Console.ReadLine());
+            cuenta.Depositar(deposito);
+
+            // Retirar dinero
+            Console.Write("\nIngrese la cantidad a retirar: $");
+            double retiro = double.Parse(Console.ReadLine());
+            cuenta.Retirar(retiro);
+
+            // Consultar saldo final
+            cuenta.ConsultarSaldo();
+
+            Console.ReadKey();
+        }
+    }
+}
